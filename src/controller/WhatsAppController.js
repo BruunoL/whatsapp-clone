45f55df0befea0ac8c04c2b4c31111f15f2620b0
Bruno Luiz <brunoluiz137@outlook.com>
2021-03-11 22:26:01 -0300

    Adicionando FormData e puxando os dados dos paineis de User e Contato.

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -67,6 +67,22 @@ class WhatsAppController {
         Element.prototype.hasClass = function(name) {
             return this.classList.contains(name);
         }
+
+        HTMLFormElement.prototype.getForm = function () {
+            return new FormData(this);
+        }
+
+        HTMLFontElement.prototype.toJSON = function() {
+
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+
+                json[key] = value;
+
+            });
+            return json;
+        }
     }
 
     initEvents() {
@@ -94,10 +110,38 @@ class WhatsAppController {
         this.el.btnClosePanelEditProfile.on('click', e => {    
             this.el.panelEditProfile.removeClass('open');
         });
+        
         this.el.btnClosePanelAddContact.on('click', e => {
             this.el.panelAddContact.removeClass('open');
         });
 
+        this.el.photoContainerEditProfile.on('click', e => {
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+            
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+            
+         });
+
+         this.el.btnSavePanelEditProfile.on('click', e => {
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML);
+
+         });
+
+         this.el.formPanelAddContact.on('submit', e => {
+            
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact)
+
+            
+         });
     }
 
     closeAllLeftPanel() {
