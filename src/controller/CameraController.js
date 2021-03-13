729b5fc1492f2698d9c8626fb7cb6e29310527a4
Bruno Luiz <brunoluiz137@outlook.com>
2021-03-13 04:00:29 -0300

    Adicionando as funções e parametrizações do webpack + adicionando as funções da camera como salvar a imagem com canvas.

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -1,4 +1,4 @@
-class CameraController {
+export class CameraController {
 
     constructor(videoEl) {
 
@@ -8,6 +8,7 @@ class CameraController {
             video: true
         }).then(stream => {
 
+            this._stream = stream;
             this._videoEl.src = URL.createObjectURL(stream);
             this._videoEl.play();
 
@@ -15,5 +16,25 @@ class CameraController {
             console.error(err);
         });
 
+    }
+
+    stop() {
+        this._stream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+
+    takePicture(mimeType = 'image/png') {
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this.videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
     }
 }
